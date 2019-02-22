bd3185db3c69f815c6d8f22c4027264092c61da7
Suhaib Mohammed <suhaib@ebi.ac.uk>
2019-02-22 17:15:20 +0000

    args passing and downloading files implemented

--- run_galaxy_workflow.py
+++ run_galaxy_workflow.py
@@ -5,29 +5,30 @@
 import argparse
 import logging
 import os.path
-import re
 import time
 import yaml
 from bioblend.galaxy import GalaxyInstance
-from pprint import pprint
 
 def get_args():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('-C', '--conf',
                             required=True,
-                            help='A yaml file describing the workflow')
+                            help='A yaml file describing the galaxy credentials')
     arg_parser.add_argument('-P', '--experimentDir',
                             default='',
+                            required=True,
                             help='Path to experiment directory folder')
     arg_parser.add_argument('-I', '--instance',
                             default='embassy',
                             help='Galaxy server instance name')
     arg_parser.add_argument('-H', '--history',
-                            default='scanpy',
+                            default='',
+                            required=True,
                             help='Name of the history to create')
-    arg_parser.add_argument('-V', '--variable',
-                            default=[],
-                            help='A list of tool parameters to vary')
+    arg_parser.add_argument('-W', '--workflow',
+                            default='Scanpy_workflow_test',
+                            required=True,
+                            help='Workflow to run')
     arg_parser.add_argument('--debug',
                             action='store_true',
                             default=False,
@@ -44,8 +45,8 @@ def set_logging_level(debug=False):
         datefmt='%y-%m-%d %H:%M:%S')
 
 
-def get_instance(name='__default'):
-    with open(os.path.expanduser('~/Workflows/bioblend_galaxy/cloud_credentials.yml'), mode='r') as fh:
+def get_instance(conf, name='__default'):
+    with open(os.path.expanduser(conf), mode='r') as fh:
         data = yaml.load(fh)
     assert name in data, 'unknown instance'
     entry = data[name]
@@ -70,17 +71,17 @@ def validate_workflow_tools(wf, tools):
         assert tool['id'] in tool_ids, "unknown tool: {}".format(tool['id'])
     return True
 
-def get_history_id (history_name, histories_obj):
+def get_history_id(history_name, histories_obj):
     for hist_dict in histories_obj:
         if hist_dict['name'] == history_name:
             return(hist_dict['id'])
 
-def upload_datasets_from_folder(experimentDir, history_name, history):
+def upload_datasets_from_folder(gi, experimentDir, history_name, history):
     expAcc = os.path.basename(experimentDir)
     files = os.listdir(experimentDir)
-    history_id = get_history_id(history_name, history)
+    history_id = history['id']
     datasets = []
-    # uploading each file from the experiment directory to history id
+    # uploading each file from the experiment directory to a history id
     # and record appended files history
     for file in files:
         logging.info('Uploading',file,'for', expAcc, '...')
@@ -90,6 +91,11 @@ def upload_datasets_from_folder(experimentDir, history_name, history):
         data = gi.tools.upload_file(path=os.path.join(experimentDir,file), history_id = history_id, file_name = file, file_type = file_type)
         datasets.append(data.items())
     return datasets
+
+def get_workflow_id(wf):
+    for wf_dic in wf:
+        wf_id = wf_dic['id']
+    return wf_id
 
 def get_input_data_id(file, wf):
     file_name = os.path.splitext(file)[0].split('_')[1]
@@ -109,52 +115,70 @@ def make_data_map(experimentDir, datasets, show_workflow):
     if isinstance(datamap, dict):
         return(datamap)
 
-def get_workflow_id(workflow):
-    for wf_dic in workflow:
-        workflow_id = wf_dic['id']
-    return workflow_id
 
-conf="/Users/suhaib/Workflows/bioblend_galaxy/bioblend-galaxy-sc-tertiary/test/scanpy_workflow.yml"
-history_name='scanpy_test1'
-debug=True
-experimentDir='/Users/suhaib/Workflows/bioblend_galaxy/E-MTAB-101'
+def get_run_state(gi, results):
+    results_hid = gi.histories.show_history(results['history'])
+    state = results_hid['state']
+    return state
 
+
+def download_results(gi, results, experimentDir):
+    results_hid = gi.histories.show_history(results['history'])
+    ok_state_ids = results_hid['state_ids']['ok']
+    for state_id in ok_state_ids:
+        gi.datasets.download_dataset(state_id, file_path=experimentDir, use_default_filename=True)
 
 def main():
     args = get_args()
     set_logging_level(args.debug)
 
+    print('Prepare environment...')
     # Prepare environment
-    ins = get_instance(name='embassy')
+    ins = get_instance(args.conf, name='embassy')
     gi = GalaxyInstance(ins['url'], key=ins['key'])
 
+    print('Create new history to run workflow ...')
     # Create new history to run workflow
-    history = gi.histories.create_history(name=history_name)
+    history = gi.histories.create_history(name=args.history)
 
-    # TODO if the history already existing retrieve history
-    #history = gi.histories.get_histories(name=history_name)
-    #pprint(history)
-
+    print('Uploading dataset to history ...')
     # upload dataset to history
-    datasets = upload_datasets_from_folder(experimentDir, history_name, history)
+    datasets = upload_datasets_from_folder(gi, args.experimentDir, args.history, history)
 
-    # get workflow defined in the galaxy
-    workflow = get_workflow_from_name(gi, workflow_name = 'Scanpy_workflow_test')
-    workflow_id = get_workflow_id(workflow)
+    print(args.workflow,'- Workflow setting ...')
+    # get saved workflow defined in the galaxy instance
+    workflow = get_workflow_from_name(gi, workflow_name = args.workflow)
+    workflow_id = get_workflow_id(wf = workflow)
     show_wf =gi.workflows.show_workflow(workflow_id)
 
-    # datamap
-    datamap = make_data_map(experimentDir,datasets, show_wf)
+    print('Datamap linking uploaded inputs and workflow inputs ...')
+    # create input datamap linking uploaded inputs and workflow inputs
+    datamap = make_data_map(args.experimentDir, datasets, show_wf)
 
+    print('Running workflow ...')
     ## run work flow
-    results = gi.workflows.run_workflow( workflow_id , datamap, history_name = (history_name + '_results'))
+    results = gi.workflows.run_workflow(workflow_id, datamap, history_name = (args.history + '_results'))
 
-    pprint(results)
-    # wait for a little while between each job submission
+    # wait for a little while and check if the status is ok
     time.sleep(20)
 
-    ## TODO download results
-    return 0
+    # get_run_state
+    state = get_run_state(gi, results)
+
+   # wait until the jobs are completed
+    while state == 'queued':
+        state = get_run_state(gi, results)
+        if state == 'queued':
+            time.sleep(10)
+            continue
+        elif state == 'ok':
+            print("jobs ok")
+            break
+
+    # Download results
+    print('Downloading results ...')
+    download_results(gi, results, experimentDir = args.experimentDir)
 
 if __name__ == '__main__':
     main()
+
