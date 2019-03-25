740584408a72ce4a142bae60b76851257a6e4cc1
Suhaib Mohammed <suhaib@ebi.ac.uk>
2019-03-25 14:24:00 +0000

    paramters file injection and submitting to Galaxy added

--- run_galaxy_workflow.py
+++ run_galaxy_workflow.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 """run_galaxy_workflow
 """
+## This script run workflows on galaxy instance using credentials.yml file provided.
+## The input data (barcodes.tsv, genes.tsv, matrix.mtx and gtf) files are upload from
+##  provisioned locally in a directory. This scripts connects to galaxy instance and select the workflow of interest.
+
+## running syntax
+## python run_galaxy_workflow.py -C galaxy_credentials.yml -D E-MTAB-101 -I 'embassy' -H 'scanpy_param_test' -W Galaxy-Workflow-Scanpy_default_params.json -P scanpy_param_pretty.json
+
+## E-MTAB-101 the experiment directory contains barcodes.tsv, genes.tsv, matrix.mtx and gtf.gz
 
 import argparse
 import logging
 import os.path
 import time
 import yaml
+import sys
+import pickle
+import copy
+import json
 from bioblend.galaxy import GalaxyInstance
 
 def get_args():
@@ -14,7 +26,7 @@ def get_args():
     arg_parser.add_argument('-C', '--conf',
                             required=True,
                             help='A yaml file describing the galaxy credentials')
-    arg_parser.add_argument('-P', '--experimentDir',
+    arg_parser.add_argument('-D', '--experimentDir',
                             default='',
                             required=True,
                             help='Path to experiment directory folder')
@@ -26,9 +38,13 @@ def get_args():
                             required=True,
                             help='Name of the history to create')
     arg_parser.add_argument('-W', '--workflow',
-                            default='Scanpy_workflow_test',
+                            default='scanpy_workflow_test',
                             required=True,
                             help='Workflow to run')
+    arg_parser.add_argument('-P', '--parameters',
+                            default='',
+                            required=True,
+                            help='scanpy parameters json')
     arg_parser.add_argument('--debug',
                             action='store_true',
                             default=False,
@@ -39,10 +55,9 @@ def get_args():
 
 def set_logging_level(debug=False):
     logging.basicConfig(
-        level=logging.DEBUG if debug else logging.WARNING,
-        format=('%(asctime)s; %(levelname)s; %(filename)s; %(funcName)s(): '
-                '%(message)s'),
-        datefmt='%y-%m-%d %H:%M:%S')
+        level=logging.DEBUG if debug else logging.INFO,
+        format=('%(asctime)s - %(message)s'),
+        datefmt='%d-%m-%y %H:%M:%S')
 
 
 def get_instance(conf, name='__default'):
@@ -56,20 +71,18 @@ def get_instance(conf, name='__default'):
         return data[entry]
 
 
-def read_workflow_from_file(filename):
-    with open(filename, mode='r') as fh:
-        wf = yaml.load(fh)
-    return wf
+def get_workflow_from_file(gi, workflow_file):
+    import_workflow = [gi.workflows.import_workflow_from_local_path(file_local_path = workflow_file)]
+    return import_workflow
+
+def read_json_file(json_file_path):
+    with open(json_file_path) as json_file:
+        json_obj = json.load(json_file)
+    return json_obj
 
 def get_workflow_from_name(gi, workflow_name):
     wf = gi.workflows.get_workflows(name=workflow_name)
     return wf
-
-def validate_workflow_tools(wf, tools):
-    tool_ids = set([tl['id'] for tl in tools])
-    for tool in wf['steps']:
-        assert tool['id'] in tool_ids, "unknown tool: {}".format(tool['id'])
-    return True
 
 def get_history_id(history_name, histories_obj):
     for hist_dict in histories_obj:
@@ -77,16 +90,18 @@ def get_history_id(history_name, histories_obj):
             return(hist_dict['id'])
 
 def upload_datasets_from_folder(gi, experimentDir, history_name, history):
-    expAcc = os.path.basename(experimentDir)
-    files = os.listdir(experimentDir)
-    history_id = history['id']
-    datasets = []
+    if os.path.exists(experimentDir):
+        expAcc = os.path.basename(experimentDir)
+        files = os.listdir(experimentDir)
+        history_id = history['id']
+
     # uploading each file from the experiment directory to a history id
     # and record appended files history
+    datasets = []
     for file in files:
-        logging.info('Uploading',file,'for', expAcc, '...')
+        print "Uploading %s ..." %(file)
         file_type = file.split(".")[-1]
-        if file_type == 'mtx':
+        if file_type == 'mtx' or file_type == 'gz':
             file_type = 'auto'
         data = gi.tools.upload_file(path=os.path.join(experimentDir,file), history_id = history_id, file_name = file, file_type = file_type)
         datasets.append(data.items())
@@ -98,7 +113,7 @@ def get_workflow_id(wf):
     return wf_id
 
 def get_input_data_id(file, wf):
-    file_name = os.path.splitext(file)[0].split('_')[1]
+    file_name = os.path.splitext(file)[0]
     for id in wf['inputs']:
         if wf['inputs'][id]['label'] == file_name:
             input_id = id
@@ -117,68 +132,95 @@ def make_data_map(experimentDir, datasets, show_workflow):
 
 
 def get_run_state(gi, results):
-    results_hid = gi.histories.show_history(results['history'])
+    results_hid = gi.histories.show_history(results['history_id'])
     state = results_hid['state']
     return state
 
 
 def download_results(gi, results, experimentDir):
-    results_hid = gi.histories.show_history(results['history'])
+    results_hid = gi.histories.show_history(results['history_id'])
     ok_state_ids = results_hid['state_ids']['ok']
     for state_id in ok_state_ids:
         gi.datasets.download_dataset(state_id, file_path=experimentDir, use_default_filename=True)
 
+def set_params(json_wf, param_data):
+    params = {}
+    for param_step_name in param_data:
+        step_ids = (key for key, value in json_wf['steps'].iteritems() if value['label'] == str(param_step_name))
+        for step_id in step_ids:
+            params.update({step_id: param_data[param_step_name]})
+    return params
+
+
 def main():
-    args = get_args()
-    set_logging_level(args.debug)
+    try:
+        args = get_args()
+        set_logging_level(args.debug)
 
-    print('Prepare environment...')
-    # Prepare environment
-    ins = get_instance(args.conf, name='embassy')
-    gi = GalaxyInstance(ins['url'], key=ins['key'])
+        # Prepare environment
+        logging.info('Prepare galaxy environment ...')
+        ins = get_instance(args.conf, name=args.instance)
+        gi = GalaxyInstance(ins['url'], key=ins['key'])
 
-    print('Create new history to run workflow ...')
-    # Create new history to run workflow
-    history = gi.histories.create_history(name=args.history)
+        # Create new history to run workflow
+        logging.info('Create new history to run workflow ...')
+        history = gi.histories.create_history(name=args.history)
 
-    print('Uploading dataset to history ...')
-    # upload dataset to history
-    datasets = upload_datasets_from_folder(gi, args.experimentDir, args.history, history)
+        # upload dataset to history
+        logging.info('Uploading dataset to history ...')
+        datasets = upload_datasets_from_folder(gi, args.experimentDir, args.history, history)
 
-    print(args.workflow,'- Workflow setting ...')
-    # get saved workflow defined in the galaxy instance
-    workflow = get_workflow_from_name(gi, workflow_name = args.workflow)
-    workflow_id = get_workflow_id(wf = workflow)
-    show_wf =gi.workflows.show_workflow(workflow_id)
+        # get saved workflow defined in the galaxy instance
+        logging.info('Workflow setup ...')
+        json_wf = read_json_file(args.workflow)
+        workflow = get_workflow_from_file(gi, workflow_file = args.workflow)
+        workflow_id = get_workflow_id(wf = workflow)
+        show_wf = gi.workflows.show_workflow(workflow_id)
 
-    print('Datamap linking uploaded inputs and workflow inputs ...')
-    # create input datamap linking uploaded inputs and workflow inputs
-    datamap = make_data_map(args.experimentDir, datasets, show_wf)
+        # create input datamap dictionary linking uploaded inputs files with workflow inputs
+        logging.info('Datamap linking uploaded inputs and workflow inputs ...')
+        datamap = make_data_map(args.experimentDir, datasets, show_wf)
 
-    print('Running workflow ...')
-    ## run work flow
-    results = gi.workflows.run_workflow(workflow_id, datamap, history_name = (args.history + '_results'))
+        # set parameters
+        logging.info('Set parameters ...')
+        param_data = read_json_file(args.parameters)
+        params = set_params(json_wf, param_data)
 
-    # wait for a little while and check if the status is ok
-    time.sleep(20)
+        logging.info('Running workflow ...')
+        results = gi.workflows.invoke_workflow(workflow_id = workflow_id, inputs = datamap,
+                                               params = params,
+                                               history_name = (args.history + '_results'))
 
-    # get_run_state
-    state = get_run_state(gi, results)
+        binary_file = open(os.path.join(args.experimentDir, args.history + '_results.bin'), mode = 'wb')
+        pickle.dump(results, binary_file)
+        binary_file.close()
 
-   # wait until the jobs are completed
-    while state == 'queued':
+        # wait for a little while and check if the status is ok
+        time.sleep(100)
+
+        # get_run_state
         state = get_run_state(gi, results)
-        if state == 'queued':
-            time.sleep(10)
-            continue
-        elif state == 'ok':
-            print("jobs ok")
-            break
 
-    # Download results
-    print('Downloading results ...')
-    download_results(gi, results, experimentDir = args.experimentDir)
+       # wait until the jobs are completed
+        while state == 'queued':
+            state = get_run_state(gi, results)
+            if state == 'queued':
+                time.sleep(10)
+                continue
+            elif state == 'ok':
+                logging.info("jobs ok")
+                break
+
+        # Download results
+        logging.info('Downloading results ...')
+        download_results(gi, results, experimentDir = args.experimentDir)
+        sys.exit(0)
+
+    except:
+         sys.exit(1)
 
 if __name__ == '__main__':
     main()
+
+
 
